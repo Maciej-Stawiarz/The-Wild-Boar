71e79fb116510f784c3862497305d14430c4a59a
Maciej-Stawiarz <precariousDuck@protonmail.com>
2025-12-03 14:18:46 +0100

    Initial commit

---
+++ src/main/java/ms/TheWildBoar/TheWildBoarApplication.java
@@ -0,0 +1,13 @@
+package ms.TheWildBoar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheWildBoarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheWildBoarApplication.class, args);
+	}
+
+}
